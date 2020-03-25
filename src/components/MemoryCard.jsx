29739432cd0e0de39cd8ede664c0e4b7012faa20
Zach Barbre <zach.barbre@gmail.com>
2020-03-25 11:34:13 -0400

    card flipping working

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor(){
+        super();
+        this.state = { isFlipped: false };
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler = () => {
+        this.setState(
+           { isFlipped: !this.state.isFlipped }
+        )
+    } 
+
     render () {
+        const memoryCardInnerClass = this.state.isFlipped? 'MemoryCardInner flipped' : 'MemoryCardInner';
+
         return (
-            <div className='MemoryCard'>
-                <div className='MemoryCardInner'>
+            <div className='MemoryCard' onClick={this.clickHandler}>
+                <div className={memoryCardInnerClass}>
                     <div className='MemoryCardBack'>
                         <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' 
                         alt='DigitalCrafts Logo'/>
